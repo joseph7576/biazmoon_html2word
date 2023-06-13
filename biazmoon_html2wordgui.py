bbf0a8ae57986073ba482fbbc00f485fb6ab7e68
joe <joeho7576@gmail.com>
2023-06-13 17:54:01 +0330

    add features to gui - only on windows

--- biazmoon_html2wordgui.py
+++ biazmoon_html2wordgui.py
@@ -2,6 +2,9 @@ import platform
 import PySimpleGUI as psg
 from docx import Document
 from bs4 import BeautifulSoup
+from docx.enum.style import WD_STYLE_TYPE
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+import os
 
 psg.theme('DarkBlue')
 layout = [[psg.T("")],
@@ -11,32 +14,44 @@ layout = [[psg.T("")],
 window = psg.Window('Biazmoon html2word', layout, size=(550,150))
 
 
+def open_file_explorer(directory):
+    path = os.path.realpath(directory)
+    os.startfile(path)
+
 def create_word_doc(file, directory):
     document = Document()
-    
+    mystyle = document.styles.add_style('mystyle', WD_STYLE_TYPE.CHARACTER)
     with open(file, encoding='utf8') as fp: # you should replace your file with mine
         soup = BeautifulSoup(fp, 'html.parser')
         question_and_choices_list  = list(soup.find_all(['p', 'span']))
 
-    CHOICE_LIST = ['الف) ', 'ب) ', 'ج) ', 'د) '] # choice list for questions
+    CHOICE_LIST = [' (الف', ' (ب', ' (ج', ' (د'] # choice list for questions
 
     qn = 1 # question number
     cn = 0 # choice number(index)
     for tag in question_and_choices_list:
         if tag.name == 'p': # if it's question
             line = f'\n{qn}- {tag.text.strip()}'
-            run = document.add_paragraph(line) # write to doc file
+            run = document.add_paragraph() # write to doc file
+            run.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+            run = run.add_run(line)
+            run.style = mystyle
+            font = run.font
+            font.rtl = True
             qn += 1
         elif tag.name == 'span': # if it's choice
             run = document.add_paragraph()
+            run.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+            r = run.add_run(tag.text.strip())
+            r.style = mystyle
+            font = r.font
+            font.rtl = True
             if cn >= len(CHOICE_LIST): # reset choice index
                 cn = 0
             if tag.has_attr('style'):
-                # \033[1mBOLD\033[0m -> BOLD ANSI code
-                run.add_run('BOLD -> ' + CHOICE_LIST[cn]).bold = True
+                r = run.add_run(CHOICE_LIST[cn]).bold = True
             else:
-                run.add_run(CHOICE_LIST[cn])
-            run.add_run(tag.text.strip())
+                r = run.add_run(CHOICE_LIST[cn])
             cn += 1
     
     sysinfo = platform.system()
@@ -56,3 +71,4 @@ while True:
         directory = psg.popup_get_folder("Choose a directory to save your docx file.", title='Where to save?')
         create_word_doc(values['-htmlfile-'], directory)
         psg.popup("needtofix.docx has created. Now go and fix that doc.", title=':D')
+        open_file_explorer(directory)
